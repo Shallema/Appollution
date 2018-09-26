a0cf5d37a6633605a491d378e0bee90f06d2970a
Sandrine <sandrine.hallemans@gmail.com>
2018-09-26 07:02:39 +0200

    ListView Adapter in ResultFrragment with errors

--- app/src/main/java/com/example/bauwensn/appollutiontest/Fragments/ResultFragment.java
+++ app/src/main/java/com/example/bauwensn/appollutiontest/Fragments/ResultFragment.java
@@ -1,18 +1,39 @@
 package com.example.bauwensn.appollutiontest.Fragments;
 
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.bauwensn.appollutiontest.R;
+import com.example.bauwensn.appollutiontest.models.api.PollutionInfo;
+import com.example.bauwensn.appollutiontest.webapi.RequestAPI;
+
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class ResultFragment extends Fragment {
+
+    private View vue;
+    private Context context;
+
+    private ListView resultsLV;
+
+    private RequestAPI requestAPI;
 
 
     public ResultFragment() {
@@ -23,8 +44,43 @@ public class ResultFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_result, container, false);
+        vue = inflater.inflate(R.layout.fragment_result, container, false);
+
+        resultsLV = vue.findViewById(R.id.lv_results);
+
+        return vue;
     }
+
+    public void displayResult() {
+        requestAPI = new RequestAPI();
+        requestAPI.setCallback(this.getContext());
+        requestAPI.execute();
+    }
+
+    @Override
+    public void onRequestResult(JSONObject data) {
+
+        Log.i("EVENT", "Finish");
+
+        List<HashMap<String, String>> adapterSource = new ArrayList<>();
+        for (PollutionInfo pollutionData : data.getDeviceID()) {
+            HashMap<String, String> element = new HashMap<>();
+            element.put("co2", pollutionData.getDioxCarb());
+            element.put("TVOC", pollutionData.getTvoc());
+            adapterSource.add(element);
+        }
+
+        SimpleAdapter adapter = new SimpleAdapter(
+                this.getContext(),
+                adapterSource,
+                android.R.layout.simple_list_item_2,
+                new String[]{"co2", "TVOC"},
+                new int[]{android.R.id.text1, android.R.id.text2}
+        );
+
+        resultsLV.setAdapter(adapter);
+    }
+
+
 
 }
